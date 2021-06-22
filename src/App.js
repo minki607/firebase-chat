fddbc4e511c5b6b1c62d63c7655b624a83d83da6
Min Gee Suh <suh@student.unimelb.edu.au>
2021-06-22 10:49:05 +0900

    feat: initial setup + react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ChatPage from "./page/ChatPage/ChatPage";
+import LoginPage from "./page/LoginPage/LoginPage";
+import RegisterPage from "./page/RegisterPage/RegisterPage";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={ChatPage} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/register" component={RegisterPage} />
+      </Switch>
+    </Router>
   );
 }
 
